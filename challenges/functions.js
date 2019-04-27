feb89bad6d135db99516051899c150ed707792c0
Junior Dugue <jrdugue@gmail.com>
2019-04-27 13:48:12 -0400

    completed functions assignement

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,9 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(param1, param2, cb) {
+  return cb(param1, param2);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,18 +17,36 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function greeting(first, last) {
+  return `Hello ${first} ${last}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
+console.log(consume(2,2, add)); 
+console.log(consume(10,16, multiply));
+console.log(consume("Mary", "Poppins", greeting));
 
 // ==== Closures ==== 
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation:
+{/*
+A nested element/block has access to any variables that are declared & assigned within the parent element/block as long as the variable is declared and assigned above the nested block. If the variable is a var or a glolbal, the nested element will have access to the declared variable no matter where it is declared, but will
+only have access to the assignment if it happens above the nested element which is due to JS hoisting the variable but not the assignment.
+*/}
 
 
 const external = "I'm outside the function";
